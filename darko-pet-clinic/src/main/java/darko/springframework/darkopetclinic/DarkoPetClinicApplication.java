dc4834bebdce7db05e12ff1d0ff16e0ced1e0daa
darko <darkotaomi@gmail.com>
2023-03-09 14:11:04 +0100

    Initial Project Add

---
+++ darko-pet-clinic/src/main/java/darko/springframework/darkopetclinic/DarkoPetClinicApplication.java
@@ -0,0 +1,13 @@
+package darko.springframework.darkopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkoPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarkoPetClinicApplication.class, args);
+    }
+
+}
---
+++ darko-pet-clinic/src/test/java/darko/springframework/darkopetclinic/DarkoPetClinicApplicationTests.java
@@ -0,0 +1,18 @@
+package darko.springframework.darkopetclinic;
+
+import jakarta.annotation.security.RunAs;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class DarkoPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
